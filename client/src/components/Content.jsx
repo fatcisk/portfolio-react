13b4bc93368a05f6c07154d88187be9ac388ba71
unknown <fatihcandev@gmail.com>
2022-01-02 23:01:18 +0300

    animation change

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -5,7 +5,7 @@ export default function Content({ content, ad }) {
   return (
     <div className="content" style={{ animationDelay: ad }}>
       <a href={`/portfolio/${content.id}`}>
-        <img src={content.images[0]} alt="project" loading="lazy" />
+        <img src={content.images[0]} alt="project" />
         <div className="title">{content.title}</div>
         <div className="tags">
           {content.tags.slice(0, 3).map((tag) => (
