f56c7c44728092515a40633c05cfd1e625b63343
Fatih Can <fatihcandev@gmail.com>
2022-02-13 10:52:31 +0300

    Add files via upload

--- client/src/pages/ContentPage.jsx
+++ client/src/pages/ContentPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./ContentPage.css";
 import { useParams } from "react-router-dom";
 import data from "../data/db.json";
-import { BsFillRecordCircleFill, BsGithub } from "react-icons/bs";
+import { BsCircleFill, BsGithub } from "react-icons/bs";
 
 export default function ContentPage() {
   const { id } = useParams();
@@ -11,7 +11,7 @@ export default function ContentPage() {
     <div className="portfolio-item">
       <h1 className="item-header">{contentData.title}</h1>
       <p className="item-description">{contentData.description}</p>
-      {contentData.github != "" ? (
+      {contentData.github !== "" ? (
         <div className="project-rew">
           <h4>Check this project on:</h4>
           <a href={contentData.github} target="_blank">
@@ -19,6 +19,19 @@ export default function ContentPage() {
               <BsGithub />
             </span>
             Github
+          </a>
+        </div>
+      ) : (
+        <></>
+      )}
+      {contentData.live !== "" ? (
+        <div className="project-rew">
+          <h4>Check out this project live:</h4>
+          <a href={contentData.live} target="_blank">
+            <span>
+              <BsCircleFill color="red" />
+            </span>
+            {contentData.live}
           </a>
         </div>
       ) : (
